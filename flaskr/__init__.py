733b13098ea05ee42440f934c6a1e86ef25764f4
Balaraman <balaramanmuthupandi@gmail.com>
2021-03-29 16:09:28 +0530

    Initialize Flask App

---
+++ flaskr/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask, Response, json
+from werkzeug.exceptions import HTTPException
+import traceback
+import os
+from flask_cors import CORS
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+    app.secret_key = 'willbereplacedbyarandomstring'
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, world!'
+
+    # Error Handler
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        """Return JSON instead of HTML for errors"""
+
+        if(isinstance(e, HTTPException)):
+            # start with the correct headers and status code from the error
+            response = e.get_response()
+            # replace the body with JSON
+            response.data = json.dumps({
+                "code": e.code,
+                "name": e.name,
+                "description": e.description,
+            })
+            response.content_type = "application/json"
+        else:
+            traceback.print_exc()
+            # start with empty response object
+            response = Response(status=500)
+            # add a JSON body
+            response.data = json.dumps({
+                "code": 500,
+                "name": 'Internal Server Error',
+                "description": 'Something Went Wrong',
+            })
+            response.content_type = "application/json"
+
+        return response
+
+    from . import digitize
+    app.register_blueprint(digitize.digitize)
+    app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
+
+    app.run(host='0.0.0.0')
+    
+    return app
---
+++ flaskr/digitize.py
@@ -0,0 +1,35 @@
+
+from flask import Blueprint, request, session, Response, abort, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+digitize = Blueprint('digitize', __name__, url_prefix='/digitize')
+
+@digitize.route('/', methods=['GET'])
+def upload():
+    return  '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@digitize.route('/', methods=['POST'])
+def upload_and_extract():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(os.getenv('UPLOAD_FOLDER'), filename))
+        return 'File Uploaded'
